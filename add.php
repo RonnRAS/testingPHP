5d3fb5c561ab9d2a19eb63c84acea0ef9d7de932
unknown <vinsonronnssumilang@gmail.com>
2026-06-08 18:27:03 +0800

    my first php file

---
+++ add.php
@@ -0,0 +1,26 @@
+<?php 
+    session_start();
+
+    include("conn.php");
+
+    if ($_SERVER ['REQUEST_METHOD'] == "POST") 
+    {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $uname = $_POST['uname'];
+        $pword = $_POST['pword'];
+
+        if (!empty($uname) && !empty($pword) && !is_numeric($uname))
+        {
+            $query = "INSERT INTO `mrsci_info` (firstname, lastname, uname, pword) VALUES ('$firstname', '$lastname', '$uname', '$pword')";
+
+            mysqli_query($conn, $query);
+
+            echo "<script type='text/javascript'> alert('Success')</script>";
+        }
+        else
+        {
+            echo "<script type='text/javascript'> alert('INVALID GAGO')</script>";
+        }
+    }
+?>
